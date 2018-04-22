e975619d77866b3a53c571858f9d5e6bd959af0e
Lam Ngo <ngobasonlam@gmail.com>
2018-04-22 12:00:45 +0000

    initial setup done

--- server.py
+++ server.py
@@ -8,3 +8,20 @@ import aiy.cloudspeech
 import aiy.voicehat
 
 from bluetooth import *
+
+def main():
+    recognizer = aiy.cloudspeech.get_recognizer()
+    recognizer.expect_phrase("Hey Little Me")
+    recognizer.expect_phrase("Turn on the LED")
+    recognizer.expect_phrase("Turn off the LED")
+
+    aiy.audio.get_recorder().start()
+
+    port = 1
+    sock = BluetoothSocket(RFCOMM)
+    nb = discover_devices()
+    if len(nb) > 0:
+        sock.connect((nb[0],1))
+        
+    while True:
+        aiy.audio.say("Set up Complete. Ready!")
